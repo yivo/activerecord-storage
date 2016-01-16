3f69a9e97c983e1c9e8c3a808da91b0c2398af54
Yaroslav Konoplov <yaroslav@inbox.com>
2016-01-16 16:56:15 +0200

    Increase column limit

--- lib/activestorage/column_types.rb
+++ lib/activestorage/column_types.rb
@@ -1,7 +1,7 @@
 module ActiveStorage
   module ColumnTypes
     def storage(*args)
-      options = args.extract_options!.reverse_merge!(null: false, default: {}.to_json)
+      options = args.extract_options!.reverse_merge!(null: false, default: {}.to_json, limit: 2**9-1)
       args.each { |name| column(name, :string, options) }
     end
   end
